9cc5b967352cb89783acb4f37593c3a76651e465
Vitório A. Cavalheiro <vitoriocavalheiro03@gmail.com>
2024-08-08 21:54:17 -0300

    inital commit

---
+++ src/bank/Bank.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bank;
+
+/**
+ *
+ * @author 0041432213010
+ */
+public class Bank {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
